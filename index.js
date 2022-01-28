0aed7e2d3d455d4f93f617af5d9af83b7a849131
Roy-Gal-Git <self.roygal@gmail.com>
2022-01-28 21:48:06 +0200

    Update the app to connect to the DB

--- index.js
+++ index.js
@@ -4,11 +4,10 @@ const dotenv = require("dotenv");
 dotenv.config();
 app = express();
 
-// require("./startup/db")();
+require("./startup/db")();
 require("./startup/routes")(app);
 require("./startup/updateDb")();
 
-// Create environemnt file (".env")
 const port = process.env.PORT || 3000;
 const server = app.listen(port);
 
