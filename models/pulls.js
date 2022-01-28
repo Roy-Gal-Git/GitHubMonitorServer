86d53c755b0ff53138d1dff7f2136dba578255be
Roy-Gal-Git <self.roygal@gmail.com>
2022-01-28 21:45:00 +0200

    Create pull request model

--- models/pulls.js
+++ models/pulls.js
@@ -1,8 +1,23 @@
 const mongoose = require("mongoose");
 
 const pullSchema = new mongoose.Schema({
-  body: {
-    type: String,
+  title: { type: String },
+  html_url: { type: String },
+  pull_request_id: { type: Number },
+  number: { type: Number },
+  user: {
+    login: { type: String },
+    user_id: { type: Number },
+    html_url: { type: String },
+  },
+  body: { type: String },
+  created_at: { type: String },
+  updated_at: { type: String },
+  closed_at: { type: String },
+  merged_at: { type: String },
+  commits: {
+    href: { type: String },
+    amount: { type: Number },
   },
 });
 
