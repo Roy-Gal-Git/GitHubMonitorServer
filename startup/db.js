cdd9f8b787e302f2bca4e01d0885d4a9e9b15538
Roy-Gal-Git <self.roygal@gmail.com>
2022-01-28 21:45:16 +0200

    Modify DB connection

--- startup/db.js
+++ startup/db.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
+// const winston = require("winston");
 
+let db = "mongodb://localhost:27017/commitlogger";
 module.exports = function () {
-  mongoose.connect("mongodb://localhost:27017/commitlogger");
+  mongoose.connect(db).then(() => console.log(`Connected to ${db}...`));
 };
