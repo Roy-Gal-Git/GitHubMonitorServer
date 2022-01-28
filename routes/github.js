4935cc5ba321882a6dfe035b183a12fd3634a10f
Roy-Gal-Git <self.roygal@gmail.com>
2022-01-28 21:47:38 +0200

    Send all of the logged pull requests from the DB to the client

--- routes/github.js
+++ routes/github.js
@@ -1,25 +1,13 @@
 const { Octokit } = require("@octokit/rest");
 const mongoose = require("mongoose");
+const { Pull } = require("../models/pulls");
 const express = require("express");
 const router = express.Router();
 const octokit = new Octokit({ baseUrl: "https://api.github.com" });
 
 router.get("/", async (req, res) => {
-  const { data: pullRequests } = await octokit.request(
-    "GET /repos/{owner}/{repo}/pulls",
-    {
-      owner: process.env.OWNER,
-      repo: process.env.REPO,
-    }
-  );
-  res.send(pullRequests);
+  let loggedPullRequests = await Pull.find().lean();
+  res.send(loggedPullRequests);
 });
-
-// router.post("/", async (req, res) => {
-// TODO: update the DB
-// לבדוק האם האיידי של הפול ריקווסט קיים בפול ריקווסט אחר בדאטה בייס
-// אם כן - לדרוס את המידע של הפול ריקווסט הקיים עם החדש
-// אם לא - להוסיף את הפול ריקווסט לדאטה בייס
-// });
 
 module.exports = router;
